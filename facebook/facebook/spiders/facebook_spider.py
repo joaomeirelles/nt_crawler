41ef971fd7b74eb46cd5e310624389f15b5f524a
Amnon Bleich <amnon@timeslot.com>
2017-05-26 00:40:14 +0200

    initiate crawler

--- facebook/facebook/spiders/facebook_spider.py
+++ facebook/facebook/spiders/facebook_spider.py
@@ -6,11 +6,13 @@ class facebookSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://www.facebook.com/<some_username>',
+            'https://www.facebook.com/<some_user_name>',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         content = response.xpath('//*[@id="profile_timeline_tiles_unit_pagelets_friends"]')
+        print '-'*80
         print content
+        print '-'*80
